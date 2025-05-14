3940e03c25ca0138e2fc2539628f9889a7556ded
arinagomza <a11r221nir3@gmail.com>
2025-05-14 19:11:01 +0300

    создание нового проекта для тестов

---
+++ T4bJl3T04K4.Tests/TabletochkaTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using T4bJl3T04K4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4bJl3T04K4.Tests
+{
+    [TestClass()]
+    public class TabletochkaTests
+    {
+        
+    }
+}
